fda423dde6b8c0c1e25054e1786d4c9519f9a85b
Jessica Deen <jessicadeen@me.com>
2016-09-30 13:06:23 -0700

    xming install script

---
+++ install/xmingsetup.ps1
@@ -0,0 +1,37 @@
+#chocolatey install
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+#x11 Server install 
+$title = "x11 Server Install"
+$message = "Which x11 Server would you like to install and configure?"
+
+$xming = New-Object System.Management.Automation.Host.ChoiceDescription "&Xming", `
+    "Installs Xming."
+
+$vcxsrv = New-Object System.Management.Automation.Host.ChoiceDescription "&VcXsrv", `
+    "Installs VcXsrv."
+
+$cancel = New-Object System.Management.Automation.Host.ChoiceDescription "&Cancel", `
+    "Cancels out of install."
+
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($xming, $vcxsrv, $cancel)
+
+$result = $host.ui.PromptForChoice($title, $message, $options, 0) 
+
+switch ($result)
+    {
+        0 {
+            Write-Host "You selected Xming."
+            Write-Host "Installing Xming x11 Server."
+            choco install xming
+        }
+        1 {
+            Write-Host "You selected VcXsrv."
+            Write-Host "Installing VcXsrv x11 Server"
+            choco install vcxsrv
+        }
+        2 {
+            Write-Host "You selected the option to cancel."
+            Write-Host "Canceling and exiting installation process!";Exit
+        }
+    }
