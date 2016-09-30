bb1b5d6efd86e1c732744665ad3b15b96637d06f
Jessica Deen <jessicadeen@me.com>
2016-09-30 13:15:56 -0700

    Automated install script

--- install/xmingsetup.ps1
+++ install/xmingsetup.ps1
@@ -7,15 +7,12 @@ $message = "Which x11 Server would you like to install and configure?"
 
 $xming = New-Object System.Management.Automation.Host.ChoiceDescription "&Xming", `
     "Installs Xming."
-
 $vcxsrv = New-Object System.Management.Automation.Host.ChoiceDescription "&VcXsrv", `
     "Installs VcXsrv."
-
 $cancel = New-Object System.Management.Automation.Host.ChoiceDescription "&Cancel", `
     "Cancels out of install."
 
 $options = [System.Management.Automation.Host.ChoiceDescription[]]($xming, $vcxsrv, $cancel)
-
 $install = $host.ui.PromptForChoice($title, $message, $options, 0) 
 
 switch ($install)
@@ -23,12 +20,12 @@ switch ($install)
         0 {
             Write-Host "You selected Xming."
             Write-Host "Installing Xming x11 Server."
-            choco install xming
+            choco install xming -y
         }
         1 {
             Write-Host "You selected VcXsrv."
             Write-Host "Installing VcXsrv x11 Server"
-            choco install vcxsrv
+            choco install vcxsrv -y
         }
         2 {
             Write-Host "You selected the option to cancel."
